e5b36e3a993547c371472b4e1061db2515eddb95
furbox <furbox@gmail.com>
2021-08-23 22:29:56 -0500

    tasks management

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks = [...this.tasks, task];
+    return task;
+  }
+}
